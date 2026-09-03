609d6d20ea2643f894439a56290bf8ed8900eaaf
ferryfernandoo <ferryfernando2205@gmail.com>
2026-09-04 00:58:53 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    sourcemap: false,
+    minify: 'oxc',
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: {
+    host: true,
+    middlewareMode: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+      '/auth': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/auth/, '/auth'),
+      }
+    }
+  }
+})
